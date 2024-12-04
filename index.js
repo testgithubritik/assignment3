aa21648c8ab084cdc75b5ed1d94ac98b1109abbb
testgithubritik <161156122+testgithubritik@users.noreply.github.com>
2024-12-04 17:59:46 +0530

    assignment3

---
+++ index.js
@@ -0,0 +1,62 @@
+//1
+/*let rows = 5;
+for (let i = 1; i <= rows; i++) 
+    {
+    let pattern = ""; 
+    for (let j = 1; j <= i; j++) {
+    pattern += "*";
+    }
+    console.log(pattern); 
+ }*/
+
+
+//2
+
+/*for (let i = 1; i <= 45; i++)
+     {
+    if (i % 3 === 0 && i % 5 === 0) {
+    console.log("FizzBuzz");
+    }
+    else if (i % 3 === 0) {
+    console.log("Fizz");
+    }
+    else if (i % 5 === 0) {
+    console.log("Buzz");
+    }
+    else {
+    console.log(i);
+    }
+  }*/
+
+//3
+
+/*var numbersArray = [1, 13, 22, 123, 49];
+var sum = 0;
+for (var i = 0; i < numbersArray.length; i++) {
+  sum += numbersArray[i];
+}
+console.log("The sum of the numbers in the array is:", sum);*/
+
+
+//4
+
+/*let arr_1 = [3, 5, 22, 5, 7, 2, 45, 75, 89, 21, 2];
+let arr_2 = [9, 2, 42, 55, 71, 22, 4, 5, 90, 25, 26];
+function getArraySum(arr) 
+{
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+let totalSum = getArraySum(arr_1) + getArraySum(arr_2);
+console.log("The total sum of all elements in both arrays is:", totalSum);*/
+
+//5
+
+
+/*let arr = [43, "what", 9, true, "cannot", false, "be", 3, true];
+for (let i = arr.length - 1; i >= 0; i--) {
+  console.log(arr[i]); 
+}*/
